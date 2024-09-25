6c3335b766ad77b19addf2a20f9d98cf7c5e57f2
mnesfane <abdessalamnesfane@gmail.com>
2024-09-25 22:10:28 +0100

    done v2

--- 6kyu/towerBuilder.js
+++ 6kyu/towerBuilder.js
@@ -1,13 +1,15 @@
 function towerBuilder(nFloors) {
     let floors = []
     for(let i = 1; i <= nFloors; i++){
-        let space = ' '.repeat(nFloors - i)
-        // repeat replace a loop
-        let stars = '*'.repeat(i * 2 - 1)
-        let floor = space + stars + space
-        floors.push(floor)
+        // let space = ' '.repeat(nFloors - i)
+        // // repeat replace a loop
+        // let stars = '*'.repeat(i * 2 - 1)
+        // let floor = space + stars + space
+        // floors.push(floor)
+        floors.push(' '.repeat(nFloors - i) + '*'.repeat(i * 2 - 1) + ' '.repeat(nFloors - i))
     }
     return floors
 }
 
-console.log(towerBuilder(2));
+// towerBuilder(30).forEach(floor => console.log(floor))
+console.log(towerBuilder(30));
