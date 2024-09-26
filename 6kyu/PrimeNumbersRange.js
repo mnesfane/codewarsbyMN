f2dd7327d4caad62cc05f7cbecc1d6038cefaf4c
mnesfane <abdessalamnesfane@gmail.com>
2024-09-26 22:53:18 +0100

    PrimeNumbersRange.js done

---
+++ 6kyu/PrimeNumbersRange.js
@@ -0,0 +1,23 @@
+const {isPrime} =  require('./isPrime.js')
+
+// console.log(isPrime(9))
+
+const getPrimes = function (start, finish){
+    let primeArray = []
+    
+    start > finish ? [start, finish] = [finish, start]: null;
+    
+    if(start <= 2 && finish > 2 ) 
+    { primeArray.push(2); start = 3}
+    if(start % 2 == 0){
+        start++
+    }
+
+    for(; start <= finish; start += 2){
+        if(isPrime(start))
+            primeArray.push(start)
+    }
+    return primeArray
+}
+
+console.log(getPrimes(2, 2))
--- 6kyu/isPrime.js
+++ 6kyu/isPrime.js
@@ -2,6 +2,7 @@ function isPrime(num) {
     if(num < 2) return false;
     if(num == 2) return true;
     if(num % 2 == 0) return false;
+    // for(let i = 3; i <= num / 2; i += 2){ work too
     for(let i = 3; i <= Math.sqrt(num); i += 2){
         if(num % i == 0){
         return false
@@ -10,5 +11,6 @@ function isPrime(num) {
     return true
 }
 
-console.log(isPrime(9))
+module.exports = {isPrime}
+// console.log(isPrime(9))
   
