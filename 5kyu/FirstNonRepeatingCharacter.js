1005774a87640f5192cf67a9c52a56241c48d5ed
mnesfane <abdessalamnesfane@gmail.com>
2024-10-02 16:36:00 +0100

    another sol

--- 5kyu/FirstNonRepeatingCharacter.js
+++ 5kyu/FirstNonRepeatingCharacter.js
@@ -15,13 +15,12 @@ const firstNonRepeatingLetter = function(str) {
 }
 
 
-function firstNonRepeatingLetter(s) {
-    var t=s.toLowerCase();
-    for (var x=0;x<t.length;x++)
-      if(t.indexOf(t[x]) === t.lastIndexOf(t[x]))
-        return s[x];
-    return "";
-  }
-
+// function firstNonRepeatingLetter(s) {
+//     var t=s.toLowerCase();
+//     for (var x=0;x<t.length;x++)
+//       if(t.indexOf(t[x]) === t.lastIndexOf(t[x]))
+//         return s[x];
+//     return "";
+//   }
 
 console.log(firstNonRepeatingLetter('sTreSS'))
